6be5cc061e34bcd4e7cbf375fcce3f25873822e1
ibrahimbitar963 <ibrahim.bitar963@gmail.com>
2021-11-05 19:16:08 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:e_commerce_app/view/view_auth/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: LoginScreen(
+
+        ),
+      ),
+    );
+  }
+}
